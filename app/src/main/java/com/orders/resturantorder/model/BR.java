4c1e12f31d530c8d577871f74e9eb55761686739
Akshayg <akshayk@info-tech.co.in>
2022-03-20 22:33:14 +0530

    missing file commit

---
+++ app/src/main/java/com/orders/resturantorder/model/BR.java
@@ -0,0 +1,10 @@
+package com.orders.resturantorder.model;
+public class BR {
+    public static final int _all = 0;
+
+    public static final int contactUsViewModel = 1;
+
+    public static final int viewModel = 2;
+
+    public static final int viewmodel = 3;
+}
